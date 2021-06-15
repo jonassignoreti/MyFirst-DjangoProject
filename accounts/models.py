d60a5b7d117069379eb96403e62286834d662d4c
Jonas Signoreti <64449236+jonassignoreti@users.noreply.github.com>
2021-06-15 20:16:33 -0300

    updating

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class AlunoUnivesp(models.Model):
+    nome = models.CharField(
+        max_length=255,
+        null=False,
+        black=False
+    )
+    sobrenome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    matricula = models.CharField(
+        max_length=14,
+        null=False,
+        blank=False
+    )
+    objetos = models.Manager()
